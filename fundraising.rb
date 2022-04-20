d0a03c98bd78eed26c97db8a5c4fceda75e4eafa
Bmaharjan <bmaharjan@fiduciary-trust.com>
2022-04-20 16:49:36 -0400

    added object interactions

--- fundraising.rb
+++ fundraising.rb
@@ -34,35 +34,61 @@ class Project
 
 end
 
+class FundRequest
+
+    attr_reader :title
+
+    def initialize(title)
+        @title = title
+        @projects =[]
+    end
+
+    def add_project(a_project)
+        @projects << a_project
+    end
+
+    def request_funding
+        puts "There are #{@projects.size} projects available!!"
+        @projects.each do |p|
+            puts p
+        end
+
+        puts "\n****"
+        puts "Here are the target funds for each project: "
+        @projects.each do |p|
+            puts p.target_fund
+        end
+
+        puts "\n****"
+        puts "Lets go through the rounds of funds allocation: "
+        @projects.each do |p|
+            p.add_fund
+            p.add_fund
+            p.lost_fund
+            puts p
+        end
+
+    end
+end
+
 project1 = Project.new("Alpha",100,1000)
 project2 = Project.new("beta", 500, 2000)
 project3 = Project.new("gamma", 100, 3000)
 
-projects = [project1, project2, project3]
+projects = FundRequest.new("VC Startup Projects")
+puts projects.title
+projects.add_project(project1)
+projects.add_project(project2)
+projects.add_project(project3)
+projects.request_funding
 
-puts "There are #{projects.size} projects available!!"
-projects.each do |p|
-    puts p
-end
+project4 = Project.new("sigma", 200, 5000)
+project5 = Project.new("theta", 400, 2000)
+project6 = Project.new("omega", 1300, 7000)
 
-puts "\n****"
-puts "Here are the target funds for each project: "
-projects.each do |p|
-    puts p.target_fund
-end
-
-puts "\n****"
-puts "Lets go through the rounds of funds allocation: "
-projects.each do |p|
-    p.add_fund
-    p.add_fund
-    p.lost_fund
-    puts p
-end
-
-puts "\n******"
-puts "Project Alpha has been removed and Project Sigma has been added"
-projects.delete(project1)
-project4 = Project.new("sigma", 300, 5000)
-projects.push(project4)
-puts projects
+projects = FundRequest.new("Ask my parents for money")
+puts projects.title
+projects.add_project(project4)
+projects.add_project(project5)
+projects.add_project(project6)
+projects.request_funding
